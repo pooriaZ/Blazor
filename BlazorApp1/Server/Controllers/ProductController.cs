7bed9e72157b6336a924655e5f800e7790e9811f
pooria zareiyan <pooriazareiyan9702@gmail.com>
2023-06-18 15:24:44 +0330

    first commits

---
+++ BlazorApp1/Server/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProductController(DataContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Products>>> GetProduct()
+        {
+            var products=await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
